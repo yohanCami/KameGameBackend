e30b5b45644ae084f968a8d3018f306eba5de0f3
secadavida <secadavida@unal.edu.co>
2025-07-15 13:26:38 -0500

    feat(packs): add search and filter

--- src/models/packs.ts
+++ src/models/packs.ts
@@ -1,7 +1,39 @@
 // models/packs.ts
 import { db } from "../db"
 import { packsTable } from "../db/schema"
-import { eq } from "drizzle-orm"
+import { and, count, eq } from "drizzle-orm"
+import { packSearchSchema, PackSearchSchema } from "../schemas/packs"
+import { fullTextSearchSql, withPagination } from "./searchHelper"
+
+export const search = async (params: PackSearchSchema) => {
+	const safeParams = packSearchSchema.parse(params)
+
+	const itemName = safeParams.itemName ? safeParams.itemName.trim() : null
+	const whereCondition = and(
+		itemName ? fullTextSearchSql(packsTable.name, itemName) : undefined,
+		safeParams.packRarity
+			? eq(packsTable.rarity, safeParams.packRarity)
+			: undefined
+	)
+
+	const query = db.select().from(packsTable).where(whereCondition).$dynamic()
+	const packs = await withPagination(
+		query,
+		safeParams.page,
+		safeParams.itemsPerPage
+	)
+
+	const packsCount = await db
+		.select({ count: count() })
+		.from(packsTable)
+		.where(whereCondition)
+
+	const totalPages = Math.ceil(
+		packsCount[0].count / (safeParams.itemsPerPage || 20)
+	)
+
+	return [packs, totalPages]
+}
 
 export const getAllPacks = async () => {
 	return db.select().from(packsTable)
